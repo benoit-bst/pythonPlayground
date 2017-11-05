d40fd266212878f5e0916ce2c772f11c7e15c05f
Benoit BRISSET <benoit.brisset@gmail.com>
2017-11-05 20:24:28 +0100

    Add swig lib

---
+++ swig/exampleCall.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import example
+
+print("fact 5 : %s" %example.fact(5))
+print("my_mod 7 3 : %s" %example.my_mod(7,3))
+print("get_time : %s" %example.get_time())
